a668e59100dc0422ffe8a011f54a372bda5b7dcc
MIBlue119 <miblue119@gmail.com>
2023-03-27 00:28:05 +0800

    Feat: add a test use argument

--- mytoolbox/pdf_translator.py
+++ mytoolbox/pdf_translator.py
@@ -57,13 +57,33 @@ def translate_chunks(chunks: List[str], lang_code:str="zh-Hant", text_engine: st
         translations = list(executor.map(translate_chunk, chunks, [lang_code]*len(chunks), [text_engine]*len(chunks)))
     return translations
 
-def translate_pdf(file_path, lang_code:str="zh-Hant", text_engine: str = "gpt-3.5-turbo", num_workers:int=10):
+def translate_pdf(
+                 file_path,
+                 lang_code:str="zh-Hant",
+                 text_engine: str = "gpt-3.5-turbo",
+                 num_workers:int=10,
+                 is_test:bool=False,
+                 test_num:int=1):
+    """Translate a pdf file to a desired language.
+    
+    Args:
+        file_path (str): The path to the pdf file.
+        lang_code (str): The language code to translate to.
+            we use ISO 639-1 language code: https://www.w3schools.com/tags/ref_language_codes.asp
+        text_engine (str): The text engine to use.
+        num_workers (int): The number of workers to use.
+        is_test (bool): Whether to test the script.
+        test_num (int): The number of pages to test.
+    """
     export_file_path = Path(file_path).stem + "_translated.pdf"
     reader = PdfFileReader(file_path)
     number_of_pages = len(reader.pages)
     logger.info(f"Number of pages: {number_of_pages}")
 
     # Get the text from each page
+    if is_test:
+        # Only test on the first page
+        number_of_pages = test_num
     text_chunks = []
     for i in range(number_of_pages):
         logger.info(f"Page {i}")
@@ -103,11 +123,13 @@ def app(args):
     lang_code = args.lang_code
     text_engine = args.text_engine
     num_workers = args.num_workers
+    is_test = args.test
+    test_num = args.test_num
     openai_api_key = os.environ.get("OPENAI_API_KEY",  args.openai_api_key)
     if openai_api_key is None:
         raise ValueError("Please provide the OpenAI API key.")
     openai.api_key = openai_api_key
-    translate_pdf(file_path, lang_code, text_engine, num_workers)
+    translate_pdf(file_path, lang_code, text_engine, num_workers, is_test, test_num)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -116,5 +138,18 @@ if __name__ == "__main__":
     parser.add_argument("--text_engine", dest="text_engine", type=str, default="gpt-3.5-turbo", help="The text engine to use.")
     parser.add_argument("--openai_api_key", dest="openai_api_key", type=str, default=None, help="The OpenAI API key.")
     parser.add_argument("--num_workers", dest="num_workers", type=int, default=10, help="The number of workers to use.")
+    parser.add_argument(
+        "--test",
+        dest="test",
+        action="store_true",
+        help="If test we only translate 1 pages you can easily check",
+    )
+    parser.add_argument(
+        "--test_num",
+        dest="test_num",
+        type=int,
+        default=1,
+        help="test num for the test",
+    )  
     args = parser.parse_args()
     app(args)
