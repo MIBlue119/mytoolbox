f1cac7542b86fb9084f92b91fa1d3bd653cecb74
MIBlue119 <miblue119@gmail.com>
2023-03-27 00:18:59 +0800

    Feat: add multithreads

--- mytoolbox/pdf_translator.py
+++ mytoolbox/pdf_translator.py
@@ -4,6 +4,8 @@ Ref: https://github.com/ywchiu/largitdata/blob/master/code/Course_222.ipynb
 import os
 from pathlib import Path
 import argparse
+from concurrent.futures import ThreadPoolExecutor
+from typing import List, Tuple
 
 from nltk.tokenize import sent_tokenize
 from pypdf import PdfFileReader, PdfFileWriter
@@ -49,30 +51,40 @@ def translate_chunk(chunk_text, lang_code:str="zh-Hant", text_engine: str = "gpt
     translated_text =  parse_text_response(response, text_engine=text_engine)
     return translated_text
 
-def translate_pdf(file_path, lang_code:str="zh-Hant", text_engine: str = "gpt-3.5-turbo"):
+def translate_chunks(chunks: List[str], lang_code:str="zh-Hant", text_engine: str = "gpt-3.5-turbo", num_workers:int=10) -> List[str]:
+    """Translate a list of text chunks."""
+    with ThreadPoolExecutor(max_workers=10) as executor:
+        translations = list(executor.map(translate_chunk, chunks, [lang_code]*len(chunks), [text_engine]*len(chunks)))
+    return translations
+
+def translate_pdf(file_path, lang_code:str="zh-Hant", text_engine: str = "gpt-3.5-turbo", num_workers:int=10):
     export_file_path = Path(file_path).stem + "_translated.pdf"
     reader = PdfFileReader(file_path)
     number_of_pages = len(reader.pages)
     logger.info(f"Number of pages: {number_of_pages}")
 
     # Get the text from each page
-    text_dict = {}
+    text_chunks = []
     for i in range(number_of_pages):
         logger.info(f"Page {i}")
         page = reader.pages[i]
         text = page.extractText()
         sentences = sent_tokenize(text)
         chunks = generate_chunks(sentences)
-        text_dict[i] = chunks
+        text_chunks.append(chunks)
 
     # Translate the text
+    translated_chunks = []
+    for chunks in text_chunks:
+        translated_chunks.append(translate_chunks(chunks, lang_code, text_engine, num_workers))
+
+    # Combine the translated chunks for each page
     translated_text_dict = {}
-    for page, chunks in text_dict.items():
+    for page, chunks in enumerate(translated_chunks):
         translated_text_dict[page] = []
         for chunk in chunks:
-            translated_text = translate_chunk(chunk, lang_code, text_engine)
-            translated_text_dict[page].append(translated_text)
-    
+            translated_text_dict[page].extend(chunk)
+
     # Write the translated text to a pdf file
     writer = PdfFileWriter()
     for page, chunks in translated_text_dict.items():
@@ -90,11 +102,12 @@ def app(args):
     file_path = args.file_path
     lang_code = args.lang_code
     text_engine = args.text_engine
+    num_workers = args.num_workers
     openai_api_key = os.environ.get("OPENAI_API_KEY",  args.openai_api_key)
     if openai_api_key is None:
         raise ValueError("Please provide the OpenAI API key.")
     openai.api_key = openai_api_key
-    translate_pdf(file_path, lang_code, text_engine)
+    translate_pdf(file_path, lang_code, text_engine, num_workers)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -102,5 +115,6 @@ if __name__ == "__main__":
     parser.add_argument("--lang_code", dest="lang_code", type=str, default="zh-Hant", help="The language code to translate to.")
     parser.add_argument("--text_engine", dest="text_engine", type=str, default="gpt-3.5-turbo", help="The text engine to use.")
     parser.add_argument("--openai_api_key", dest="openai_api_key", type=str, default=None, help="The OpenAI API key.")
+    parser.add_argument("--num_workers", dest="num_workers", type=int, default=10, help="The number of workers to use.")
     args = parser.parse_args()
     app(args)
